5a2c6d68da14c678d3f794729d2776b68a0564bb
Gerardo Santovena <gsantovena@gmail.com>
2015-06-27 18:37:13 -0600

    More exercisms I finished.

---
+++ python/etl/etl.py
@@ -0,0 +1,6 @@
+def transform(text):
+    pass
+
+if __name__ == '__main__':
+    print transform('WORLD')
+
---
+++ python/grade-school/school.py
@@ -0,0 +1,44 @@
+class School(object):
+
+    def __init__(self, schoolName):
+        self.schoolName = schoolName
+        self.db = {}
+
+    def add(self, studentName, grade):
+        if self.db.has_key(grade):
+            self.db[grade].add(studentName)
+        else:
+            self.db[grade] = {studentName}
+
+    def grade(self, n):
+        if self.db.has_key(n):
+            return self.db[n]
+        else:
+            return set()
+
+    def sort(self):
+        s = {}
+        for k in sorted(self.db):
+            s[k] = set(sorted(self.db[k]))
+
+        return list(s.items() if hasattr(s, "items") else s)
+
+if __name__ == '__main__':
+    school = School("Haleakala Hippy School")
+
+    students = [
+        (3, ("Kyle",)),
+        (4, ("Christopher", "Jennifer",)),
+        (6, ("Kareem",))
+    ]
+
+    for grade, students_in_grade in students:
+        for student in students_in_grade:
+            school.add(student, grade)
+
+    result = school.sort()
+    result_list = list(result.items() if hasattr(result, "items") else result)
+
+    print result
+    print result_list
+
